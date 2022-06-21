3b078f43c63b23fc5e07157caab1c6a02998f819
fzq <fzq@qq.com>
2022-06-21 16:59:10 +0800

    second

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -3,5 +3,7 @@ public class git {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
+        System.out.println("hot-fix commit");
     }
 }
